a3443bc4f97c7f705fae554ca2e1b5142f957194
Ali <q66p@hotmail.com>
2019-08-26 12:18:27 +0300

    Second commit

--- src/app.js
+++ src/app.js
@@ -7,6 +7,7 @@ const geocode = require('./utils/geocode');
 const forcast = require('./utils/forecast');
 
 const app =  express();
+const port = process.env.PORT || 3000
 
 // Define paths for Express config
 const publicPath = path.join(__dirname, '../public');
@@ -82,6 +83,6 @@ app.get('*', (req, res) => {
         errorMessage: 'Page not found'
     })
 })
-app.listen(3000, ()=> {
-    console.log('Server is up on port 3000.')
+app.listen(port, ()=> {
+    console.log('Server is up on port' + port);
 })
